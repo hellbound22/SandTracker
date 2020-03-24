8031fc8e9a4db8301bc78ca5f7124e68a476c734
Rodrigo <rodrigomauricio@protonmail.com>
2020-03-24 16:07:23 -0300

    Plotagem feita

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,106 @@
+use plotters::prelude::*;
+
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 
 mod models;
 
-const CANVAS_SIZE: f64 = 360.0 * 60.0;
-const FOV_SIZE: f64 = 5.0 * 60.0;
+const CANVAS_SIZE: i64 = 360 * 60;
+const FOV_SIZE: i64 = 5 * 60;
+
+pub struct Canvas {
+    size: i64,
+    map: HashMap<(i64, i64), Option<models::Ponto>>,
+}
+
+impl Canvas {
+    pub fn get_vec(&self, h: i64, v: i64) -> Vec<bool> {
+        let mut r: Vec<bool> = Vec::new();
+
+        if h == -1 {
+            for x in 0..CANVAS_SIZE as i64 {
+                r.push(match self.map.get(&(x, v)) {
+                    Some(_) => true,
+                    None => false,
+                });
+            }
+        }
+
+        if v == -1 {
+            for x in 0..CANVAS_SIZE as i64 {
+                r.push(match self.map.get(&(h, x)) {
+                    Some(_) => true,
+                    None => false,
+                });
+            }
+        }
+        r
+    }
+}
 
 fn main() {
-    let mut file_dataset = File::open("./dataset/hip2.dat").unwrap();
     let mut dataset = String::new();
-    file_dataset.read_to_string(&mut dataset).unwrap();
-
-    let mut vc: Vec<models::Ponto> = Vec::new();
-
-    for linha in dataset.split_terminator('\n') {
-        vc.push(models::Ponto::new_from_line(linha).unwrap())
+    {
+        let mut file_dataset = File::open("./dataset/hip2.dat").unwrap();
+        file_dataset.read_to_string(&mut dataset).unwrap();
     }
 
-    let sec = vc.get(..100).unwrap();
+    let sec: Vec<models::Ponto>;
+    {
+        let mut vc: Vec<models::Ponto> = Vec::new();
 
-    //let mut canvas: HashMap<(i64, i64), bool> = HashMap::new();
-    let mut v_canvas = vec![false; CANVAS_SIZE as usize];
-    let mut canvas = vec![v_canvas.clone(); CANVAS_SIZE as usize];
+        for linha in dataset.split_terminator('\n') {
+            vc.push(models::Ponto::new_from_line(linha).unwrap())
+        }
+        sec = vc.get(..).unwrap().to_vec();
+    }
 
-    println!(
-        "Tamanho do canvas(espaço de plot / em arcminutos): {0} x {0}",
-        CANVAS_SIZE
-    );
+    let mut canvas: HashMap<(i64, i64), Option<models::Ponto>> = HashMap::new();
+
     println!(
         "Tamanho do campo de visão (em arcminutos): {0} x {0}",
         FOV_SIZE
     );
 
-    println!("======Posição em arcminuto======");
-    for star in sec {
-        // Posição em angulo
-        canvas[star.pos_h as usize][star.pos_v as usize] = true;
-        println!("{}, {}", star.pos_h, star.pos_v);
+    for star in &sec {
+        canvas.insert(star.coord(), Some(star.clone()));
     }
-    dbg!(&canvas[18]);
+
+    let canvas = Canvas {
+        size: CANVAS_SIZE,
+        map: canvas,
+    };
+
+    println!(
+        "Tamanho do canvas(espaço de plot / em arcminutos): {0} x {0}",
+        canvas.size
+    );
+
+    // plot em gráfico
+    let res = 2500;
+
+    let root = BitMapBackend::new("scatter.png", (res, res)).into_drawing_area();
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("y=x^2", ("sans-serif", 50).into_font())
+        .margin(20)
+        .x_label_area_size(50)
+        .y_label_area_size(50)
+        .build_ranged(-100..CANVAS_SIZE, -100..CANVAS_SIZE)
+        .unwrap();
+
+    root.fill(&WHITE).unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    chart.draw_series(PointSeries::<_, _, Circle<_, _>, _>::new(
+        sec.iter().map(|x| (x.pos_h, x.pos_v)),
+        1,
+        ShapeStyle {
+            color: BLACK.to_rgba(),
+            filled: true,
+            stroke_width: 1,
+        },
+    ));
 }
--- src/models.rs
+++ src/models.rs
@@ -9,6 +9,10 @@ pub struct Ponto {
 }
 
 impl Ponto {
+    pub fn coord(&self) -> (i64, i64) {
+        (self.pos_h, self.pos_v)
+    }
+
     pub fn new_from_line(l: &str) -> Result<Ponto, std::num::ParseFloatError> {
         let id: i32 = l
             .get(0..6)
@@ -19,16 +23,27 @@ impl Ponto {
         let r_asc: f64 = l.get(15..28).unwrap().trim().parse()?;
         let dec: f64 = l.get(29..42).unwrap().trim().parse()?;
         let plx: f64 = l.get(43..50).unwrap().trim().parse()?;
+        /*
+                let mut pos_v = ((dec * 180.0 / 3.1416) * 60.0).round() as i64; // há negativos
 
-        let mut pos_v = ((dec * 180.0 / 3.1416) * 60.0).round() as i64; // há negativos
+                let mut pos_v = {
+                    if dec < 0.0 {
+                        (((dec * 180.0 / 3.1416) * 60.0).round() as i64) * -1 + (crate::CANVAS_SIZE / 2)
+                    } else {
+                        ((dec * 180.0 / 3.1416) * 60.0).round() as i64
+                    }
+                };
+        */
+        let pos_h = (r_asc.to_degrees() * 60.0).round() as i64;
 
-        if pos_v < 0 {
-            pos_v = pos_v * -1;
-        } else if pos_v >= 0 {
-            pos_v = pos_v + (180 * 60);
+        let mut pos_v = (dec.to_degrees().abs() * 60.0);
+        if dec < 0.0 {
+            pos_v = (pos_v + ((crate::CANVAS_SIZE / 2) as f64)).round();
+        } else {
+            pos_v = ((pos_v * -1.0) + (crate::CANVAS_SIZE / 2) as f64).round();
         }
 
-        let pos_h = ((r_asc * 180.0 / 3.1416) * 60.0).round() as i64;
+        let pos_v = pos_v as i64;
 
         Ok(Ponto {
             id,
